80820eb11a78464d8274c0850eed1bb952b30455
Muzzammil Soofie <2731823@students.wits.ac.za>
2025-11-03 13:17:06 +0000

    fixing kleb mistake bec he poes dom

--- strategy/Strategy.py
+++ strategy/Strategy.py
@@ -3,29 +3,29 @@ import numpy as np
 from math_ops.Math_Ops import Math_Ops as M
 
 
-
 class Strategy():
+
     def __init__(self, world):
         self.play_mode = world.play_mode
-        self.robot_model = world.robot  
+        self.robot_model = world.robot
         self.my_head_pos_2d = self.robot_model.loc_head_position[:2]
         self.player_unum = self.robot_model.unum
-        self.mypos = (world.teammates[self.player_unum-1].state_abs_pos[0],world.teammates[self.player_unum-1].state_abs_pos[1])
-       
+        self.mypos = (world.teammates[self.player_unum-1].state_abs_pos[0],
+                      world.teammates[self.player_unum-1].state_abs_pos[1])
+
         self.side = 1
         if world.team_side_is_left:
             self.side = 0
 
-        self.teammate_positions = [teammate.state_abs_pos[:2] if teammate.state_abs_pos is not None 
-                                    else None
-                                    for teammate in world.teammates
-                                    ]
-        
-        self.opponent_positions = [opponent.state_abs_pos[:2] if opponent.state_abs_pos is not None 
-                                    else None
-                                    for opponent in world.opponents
-                                    ]
+        self.teammate_positions = [teammate.state_abs_pos[:2] if teammate.state_abs_pos is not None
+                                 else None
+                                 for teammate in world.teammates
+                                 ]
 
+        self.opponent_positions = [opponent.state_abs_pos[:2] if opponent.state_abs_pos is not None
+                                 else None
+                                 for opponent in world.opponents
+                                 ]
 
         self.team_dist_to_ball = None
         self.team_dist_to_oppGoal = None
@@ -36,109 +36,96 @@ class Strategy():
         self.point_preferences = None
         self.combined_threat_and_definedPositions = None
 
-
         self.my_ori = self.robot_model.imu_torso_orientation
         self.ball_2d = world.ball_abs_pos[:2]
         self.ball_vec = self.ball_2d - self.my_head_pos_2d
         self.ball_dir = M.vector_angle(self.ball_vec)
         self.ball_dist = np.linalg.norm(self.ball_vec)
-        self.ball_sq_dist = self.ball_dist * self.ball_dist 
+        self.ball_sq_dist = self.ball_dist * self.ball_dist
         self.ball_speed = np.linalg.norm(world.get_ball_abs_vel(6)[:2])
-        
-        self.goal_dir = M.target_abs_angle(self.ball_2d,(15.05,0))
+
+        self.goal_dir = M.target_abs_angle(self.ball_2d, (15.05, 0))
 
         self.PM_GROUP = world.play_mode_group
 
-        self.slow_ball_pos = world.get_predicted_ball_pos(0.5) 
+        self.slow_ball_pos = world.get_predicted_ball_pos(0.5)
 
-        
-        self.teammates_ball_sq_dist = [np.sum((p.state_abs_pos[:2] - self.slow_ball_pos) ** 2)  
-                                  if p.state_last_update != 0 and (world.time_local_ms - p.state_last_update <= 360 or p.is_self) and not p.state_fallen
-                                  else 1000 
-                                  for p in world.teammates ]
+        self.teammates_ball_sq_dist = [np.sum((p.state_abs_pos[:2] - self.slow_ball_pos) ** 2)
+                                     if p.state_last_update != 0 and (world.time_local_ms - p.state_last_update <= 360 or p.is_self) and not p.state_fallen
+                                     else 1000
+                                     for p in world.teammates]
 
-        
-        self.opponents_ball_sq_dist = [np.sum((p.state_abs_pos[:2] - self.slow_ball_pos) ** 2)  
-                                  if p.state_last_update != 0 and world.time_local_ms - p.state_last_update <= 360 and not p.state_fallen
-                                  else 1000 
-                                  for p in world.opponents ]
+        self.opponents_ball_sq_dist = [np.sum((p.state_abs_pos[:2] - self.slow_ball_pos) ** 2)
+                                     if p.state_last_update != 0 and world.time_local_ms - p.state_last_update <= 360 and not p.state_fallen
+                                     else 1000
+                                     for p in world.opponents]
 
         self.min_teammate_ball_sq_dist = min(self.teammates_ball_sq_dist)
-        self.min_teammate_ball_dist = math.sqrt(self.min_teammate_ball_sq_dist)  
-        
+        self.min_teammate_ball_dist = math.sqrt(
+            self.min_teammate_ball_sq_dist)
+
         sorted_teammate_ball_sq_dist = sorted(self.teammates_ball_sq_dist)
         self.second_min_teammate_ball_sq_dist = sorted_teammate_ball_sq_dist[1]
-        
 
-        self.active_player_unum = self.teammates_ball_sq_dist.index(self.min_teammate_ball_sq_dist) + 1
+        self.min_opponent_ball_dist = math.sqrt(min(self.opponents_ball_sq_dist))
+
+        self.active_player_unum = self.teammates_ball_sq_dist.index(
+            self.min_teammate_ball_sq_dist) + 1
 
         self.my_desired_position = self.mypos
         self.my_desired_orientation = self.ball_dir
-        
-        
-        self.goal_position = (15.5, 0.0)  
-        self.goal_tolerance = 0.25  
-        
+
+        self.goal_position = (15.5, 0.0)
+        self.goal_tolerance = 0.25
+
         self.goal_attraction_coefficient = 5.0
         self.obstacle_repulsion_coefficient = 100.0
         self.obstacle_effect_radius = 1.0
-        self.damping_coefficient = 0.3  
-        self.previous_force = np.zeros(2)  
+        self.damping_coefficient = 0.3
+        self.previous_force = np.zeros(2)
 
-        self.formation_tiers = {"back": -10, "mid": 0, "forward": 10}  
+        self.formation_tiers = {"back": -10, "mid": 0, "forward": 10}
 
-        self.defensive_bound_x = -10  
+        self.defensive_bound_x = -10
         self.goal_shoot_margin = 0.2
-        
 
     def GenerateTeamToTargetDistanceArray(self, target, world):
         for teammate in world.teammates:
             pass
-        
 
     def IsFormationReady(self, point_preferences):
-        
         is_formation_ready = True
         num_players = len(self.teammate_positions)
-        
-        
+
         for i in range(1, num_players + 1):
-            if i != self.active_player_unum: 
+            if i != self.active_player_unum:
                 teammate_pos = self.teammate_positions[i-1]
 
                 if not teammate_pos is None:
-                    
                     if i in point_preferences:
-                        distance = np.sum((teammate_pos - point_preferences[i]) ** 2)
+                        distance = np.sum(
+                            (teammate_pos - point_preferences[i]) ** 2)
                         if(distance > 0.3):
                             is_formation_ready = False
-                    
 
         return is_formation_ready
 
-    def GetDirectionRelativeToMyPositionAndTarget(self,target):
+    def GetDirectionRelativeToMyPositionAndTarget(self, target):
         target_vec = np.array(target) - self.my_head_pos_2d
         target_dir = M.vector_angle(target_vec)
-
         return target_dir
-    
-    
-    
+
     def potential_fields_pathfinding(self, start_pos, goal_pos=None):
         if goal_pos is None:
             goal_pos = self.goal_position
 
-        
         attraction_force = self.calculate_attraction(start_pos, goal_pos)
-        
-       
         repulsion_force = self.calculate_repulsion(start_pos)
-
-        
         boundary_correction = self.apply_boundary_correction(start_pos)
 
-        
-        resulting_force = attraction_force + repulsion_force + boundary_correction + self.central_field_correction(start_pos) - self.damping_coefficient * self.previous_force
+        resulting_force = attraction_force + repulsion_force + boundary_correction + \
+            self.central_field_correction(
+                start_pos) - self.damping_coefficient * self.previous_force
         self.previous_force = resulting_force
         next_position = start_pos + resulting_force
 
@@ -148,31 +135,33 @@ class Strategy():
         direction_to_goal = np.array(goal_pos) - np.array(current_pos)
         distance_to_goal = np.linalg.norm(direction_to_goal)
         decay_factor = 1 - np.exp(-0.1 * distance_to_goal)
-        
+
         if distance_to_goal > 0:
             direction_to_goal = direction_to_goal / distance_to_goal
         else:
             direction_to_goal = np.zeros_like(direction_to_goal)
 
-        
-        boundary_proximity_boost = 1.5 if np.linalg.norm(self.apply_boundary_correction(current_pos)) > 0.5 else 1.0
-        attraction_multiplier = 1.0 if self.ball_dist > 0.5 else 1.5  
+        boundary_proximity_boost = 1.5 if np.linalg.norm(
+            self.apply_boundary_correction(current_pos)) > 0.5 else 1.0
+        attraction_multiplier = 1.0 if self.ball_dist > 0.5 else 1.5
 
-        attraction_force = self.goal_attraction_coefficient * direction_to_goal * decay_factor * attraction_multiplier * boundary_proximity_boost
+        attraction_force = self.goal_attraction_coefficient * direction_to_goal * \
+            decay_factor * attraction_multiplier * boundary_proximity_boost
         return attraction_force
 
     def calculate_repulsion(self, current_pos):
         total_repulsion = np.zeros(2)
         for opponent_pos in self.opponent_positions:
             if opponent_pos is not None:
-                direction_from_obstacle = np.array(current_pos) - np.array(opponent_pos)
+                direction_from_obstacle = np.array(
+                    current_pos) - np.array(opponent_pos)
                 distance_to_obstacle = np.linalg.norm(direction_from_obstacle)
 
                 if distance_to_obstacle < self.obstacle_effect_radius:
                     if distance_to_obstacle > 0:
                         direction_from_obstacle /= distance_to_obstacle
                     variable_repulsion = (self.obstacle_repulsion_coefficient / (distance_to_obstacle ** 2 + 0.1)) \
-                                           * (0.8 if self.ball_dist < 0.5 else 1.0)
+                        * (0.8 if self.ball_dist < 0.5 else 1.0)
                     repulsion_force = variable_repulsion * direction_from_obstacle
                     total_repulsion += repulsion_force
 
@@ -181,17 +170,18 @@ class Strategy():
     def apply_boundary_correction(self, current_pos):
         correction_force = np.zeros(2)
 
-        
-        if abs(current_pos[0]) > 13:  
+        if abs(current_pos[0]) > 13:
             distance_to_boundary_x = 15 - abs(current_pos[0])
-            correction_force[0] = -1.0 / (distance_to_boundary_x + 0.1) * np.sign(current_pos[0])
+            correction_force[0] = -1.0 / \
+                (distance_to_boundary_x + 0.1) * np.sign(current_pos[0])
 
-        if abs(current_pos[1]) > 8:  
+        if abs(current_pos[1]) > 8:
             distance_to_boundary_y = 10 - abs(current_pos[1])
-            correction_force[1] = -1.0 / (distance_to_boundary_y + 0.1) * np.sign(current_pos[1])
+            correction_force[1] = -1.0 / \
+                (distance_to_boundary_y + 0.1) * np.sign(current_pos[1])
 
         return correction_force
-    
+
     def central_field_correction(self, current_pos):
         center_of_field = np.array([0, 0])
         direction_to_center = center_of_field - np.array(current_pos)
@@ -200,97 +190,85 @@ class Strategy():
             return np.zeros(2)
         central_attraction_force = 0.05 * direction_to_center / norm
         return central_attraction_force
-        
+
     def distance(self, point1, point2):
-        return np.linalg.norm(np.array(point1) - np.array(point2))  
-    
+        return np.linalg.norm(np.array(point1) - np.array(point2))
+
     def calculate_orientation(self, target_pos, my_pos):
         current_orientation = self.my_ori
         target_vec = np.array(target_pos) - np.array(my_pos)
         orientation_radians = np.arctan2(target_vec[1], target_vec[0])
         orientation_degrees = np.degrees(orientation_radians)
         relative_orientation = orientation_degrees
-    
+
         return relative_orientation
-    
+
     def are_points_collinear(self, position, ball_pos, goal, tolerance=0.45):
         x1, y1 = position
         x2, y2 = ball_pos
         x3, y3 = goal
-        
 
         if abs(x2 - x1) < 1e-6 and abs(x3 - x2) < 1e-6:
-             return True 
+            return True
 
         if abs(x2 - x1) < 1e-6 or abs(x3 - x2) < 1e-6:
-            return False 
+            return False
 
         slope1 = (y2 - y1) / (x2 - x1)
         slope2 = (y3 - y2) / (x3 - x2)
-        
-        
+
         return abs(slope1 - slope2) < tolerance
-    
+
     def point_in_direction(self, position, goal, distance=0.2):
         x1, y1 = position
         x2, y2 = goal
 
-        direction_x = x2 - x1    
+        direction_x = x2 - x1
         direction_y = y2 - y1
 
         magnitude = math.sqrt(direction_x**2 + direction_y**2)
 
-        
         if magnitude == 0:
             return position
 
-        
         unit_direction_x = direction_x / magnitude
         unit_direction_y = direction_y / magnitude
 
-        
         scaled_x = unit_direction_x * distance
         scaled_y = unit_direction_y * distance
 
-       
         new_position = (x1 + scaled_x, y1 + scaled_y)
 
-        return new_position  
+        return new_position
 
     def next_position_to_startat(self, target, ball, myposition, startat, buffer_distance=0.4, angle_threshold=78):
-        
         target = np.array(target)
         ball = np.array(ball)
         myposition = np.array(myposition)
         startat = np.array(startat)
 
-        
         target_to_ball = ball - target
         ball_to_myposition = myposition - ball
-        
+
         norm_target_to_ball = np.linalg.norm(target_to_ball)
         norm_ball_to_mypos = np.linalg.norm(ball_to_myposition)
 
         if norm_target_to_ball == 0 or norm_ball_to_mypos == 0:
             return tuple(startat)
 
-        
         dot_product = np.dot(target_to_ball, ball_to_myposition)
         magnitude_product = norm_target_to_ball * norm_ball_to_mypos
-        
-        
+
         cos_angle = np.clip(dot_product / magnitude_product, -1.0, 1.0)
         angle_degrees = np.degrees(np.arccos(cos_angle))
 
         if abs(angle_degrees) < angle_threshold:
-            
-            perpendicular_direction = np.array([-target_to_ball[1], target_to_ball[0]]) / norm_target_to_ball
-            
-            
+            perpendicular_direction = np.array(
+                [-target_to_ball[1], target_to_ball[0]]) / norm_target_to_ball
+
             left_of_ball = ball + perpendicular_direction * buffer_distance
             right_of_ball = ball - perpendicular_direction * buffer_distance
 
-            
             distances = {
                 "left": np.linalg.norm(myposition - left_of_ball),
                 "right": np.linalg.norm(myposition - right_of_ball),
@@ -309,16 +287,15 @@ class Strategy():
 
     def get_player_ahead(self, position):
         ind = None
-        distance = float('inf')  
+        distance = float('inf')
         num_players = len(self.teammate_positions)
 
         for i in range(num_players):
-            if self.teammate_positions[i] is not None and self.teammate_positions[i][0] > position[0] + 1.5:  
-                current_distance = self.distance(self.teammate_positions[i], position)
+            if self.teammate_positions[i] is not None and self.teammate_positions[i][0] > position[0] + 1.5:
+                current_distance = self.distance(
+                    self.teammate_positions[i], position)
                 if current_distance < distance and distance > 2:
                     ind = i
                     distance = current_distance
 
         return self.teammate_positions[ind] if ind is not None else position
-    
-    
